caeff2aae113b6b54ba6d4e05b8f4239958accd3
Azulavx <martynamoczulska2004@gmail.com>
2023-05-17 02:40:43 +0200

    nienaprawione

--- pub/index.php
+++ pub/index.php
@@ -1,30 +1,36 @@
 <?php
-
-
 require_once('./../src/config.php');
 use Steampixel\Route;
-Route::add('/', function(){
+Route::add('/', function() {
+    //strona wyświetlająca obrazki
     global $twig;
-    $twig->display("index.html.twig");
-});
-
-Route::add('/upload', function(){
-    global $twig;
-     if(isset($_POST['submit'])) {
-        Post::upload($_FILES['uploadedFile']['tmp_name']);
-    }
-    $twig->display("upload.html.twig");
+   
+    //pobierz 10 najnowszych postów
+    $postArray = Post::getPage();
+    $twigData = array("postArray" => $postArray,
+                        "pageTitle" => "Strona główna");
+    $twig->display("index.html.twig", $twigData);
 });
 
 Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazków
     global $twig;
-    if(isset($_POST['submit'])) {
+    
+    $twigData = array("pageTitle" => "Wgraj mema");
+    $twig->display("upload.html.twig", $twigData);
+});
+
+Route::add('/upload', function() {
+    //wywoła się tylko po otrzymaniu danych metodą post na ten url
+    // (po wypełnieniu formularza)
+    global $twig;
+    if(isset($_POST['submit']))  {
         Post::upload($_FILES['uploadedFile']['tmp_name']);
     }
-    $twig->display("index.html.twig");
+    
+    //TODO: zmienić na ścieżkę względną
+    header("Location: http://localhost/cms/pub");
 }, 'post');
 
-
 Route::run('/cms/pub');
-
 ?>
